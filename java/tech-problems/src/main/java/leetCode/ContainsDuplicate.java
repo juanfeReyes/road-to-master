4ee4aa9a773d2e2c5d8f3a0c6f497a5caf8bae6f
juanfeReyes <juan.felipe.reyesg@gmail.com>
2022-01-22 08:46:09 -0500

    solve contains duplicate

---
+++ java/tech-problems/src/main/java/leetCode/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+
+  public boolean containsDuplicate(int[] nums) {
+    HashMap<Integer, Integer> count = new HashMap<>();
+    for (int num: nums){
+      if(count.containsKey(num)){
+        count.replace(num, count.get(num)+1);
+      }else{
+        count.put(num, 1);
+      }
+      if(count.get(num)>1){
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
---
+++ java/tech-problems/src/test/java/leetCode/ContainsDuplicateTest.java
@@ -0,0 +1,34 @@
+package leetCode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ContainsDuplicateTest {
+  ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+
+  @Test
+  public void test1(){
+    int[] input = {1,2,3,1};
+    assertThat(containsDuplicate.containsDuplicate(input)).isFalse();
+  }
+
+  @Test
+  public void test2(){
+    int[] input = {1,2,3,4};
+    assertThat(containsDuplicate.containsDuplicate(input)).isFalse();
+  }
+
+  @Test
+  public void test3(){
+    int[] input = {1,1,1,3,3,4,3,2,4,2};
+    assertThat(containsDuplicate.containsDuplicate(input)).isTrue();
+  }
+
+  @Test
+  public void test4(){
+    int[] input = {1,2,3,1};
+    assertThat(containsDuplicate.containsDuplicate(input)).isTrue();
+  }
+}
