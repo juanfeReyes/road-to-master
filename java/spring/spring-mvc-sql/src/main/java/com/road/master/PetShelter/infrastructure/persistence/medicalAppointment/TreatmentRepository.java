2c197c88bb8bb42e2c4e3325545b0e9deef06cdc
juanfeReyes <juan.felipe.reyesg@gmail.com>
2023-05-01 10:19:37 -0500

    add locking to read queries

--- java/spring/spring-mvc-sql/src/main/java/com/road/master/PetShelter/infrastructure/persistence/medicalAppointment/TreatmentRepository.java
+++ java/spring/spring-mvc-sql/src/main/java/com/road/master/PetShelter/infrastructure/persistence/medicalAppointment/TreatmentRepository.java
@@ -1,9 +1,12 @@
 package com.road.master.PetShelter.infrastructure.persistence.medicalAppointment;
 
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.LockModeType;
 
 @Repository
 public interface TreatmentRepository extends CrudRepository<TreatmentEntity, String>,
@@ -11,5 +14,6 @@ public interface TreatmentRepository extends CrudRepository<TreatmentEntity, Str
 
 
   @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, value = "complete_treatment")
+  @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
   TreatmentEntity getTreatmentById(String id);
 }
