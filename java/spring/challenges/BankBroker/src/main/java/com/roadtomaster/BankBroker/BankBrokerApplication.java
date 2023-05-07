038ec6ce14f8a6ae4ec7434a76d4383c7dbe0ebd
juanfeReyes <juan.felipe.reyesg@gmail.com>
2023-05-06 21:29:57 -0500

    feat: add basic setup for bank broker app

---
+++ java/spring/challenges/BankBroker/src/main/java/com/roadtomaster/BankBroker/BankBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.roadtomaster.BankBroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBrokerApplication.class, args);
+	}
+
+}
