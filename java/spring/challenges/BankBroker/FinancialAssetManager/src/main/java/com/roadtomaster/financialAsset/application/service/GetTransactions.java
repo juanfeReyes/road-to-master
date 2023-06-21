adec5f13db40348ce25086097396200baa276f7a
juanfeReyes <juan.felipe.reyesg@gmail.com>
2023-06-21 12:04:30 -0500

    feat: add cache to transactions and fixed local data backup

--- java/spring/challenges/BankBroker/FinancialAssetManager/src/main/java/com/roadtomaster/financialAsset/application/service/GetTransactions.java
+++ java/spring/challenges/BankBroker/FinancialAssetManager/src/main/java/com/roadtomaster/financialAsset/application/service/GetTransactions.java
@@ -5,10 +5,13 @@ import com.roadtomaster.financialAsset.domain.model.Transaction;
 import com.roadtomaster.financialAsset.infrastructure.persistence.transaction.TransactionQuery;
 import com.roadtomaster.financialAsset.infrastructure.persistence.transaction.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.StreamSupport;
+
+import static com.roadtomaster.financialAsset.infrastructure.config.CacheConfig.TRANSACTIONS_CACHE_NAME;
 
 @Service
 public class GetTransactions {
@@ -23,6 +26,7 @@ public class GetTransactions {
     this.transactionMapper = transactionMapper;
   }
 
+  @Cacheable(value = TRANSACTIONS_CACHE_NAME, key = "#userId")
   public List<Transaction> getByUser(String userId) {
     var transactions = transactionRepository.findAll(new TransactionQuery().queryByUser(userId));
     return StreamSupport.stream(transactions.spliterator(), false).
