0b41f78157b9dfd1b32643a1ac25f1bb017a08a8
juanfeReyes <juan.felipe.reyesg@gmail.com>
2023-06-20 19:26:42 -0500

    feat: add authorization to app

--- java/spring/challenges/BankBroker/BankManager/src/main/java/com/roadtomaster/bank/infrastructure/config/SecurityConfig.java
+++ java/spring/challenges/BankBroker/BankManager/src/main/java/com/roadtomaster/bank/infrastructure/config/SecurityConfig.java
@@ -2,9 +2,20 @@ package com.roadtomaster.bank.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfig {
@@ -17,10 +28,31 @@ public class SecurityConfig {
                             .permitAll()
                             .requestMatchers("/v3/api-docs/**")
                             .permitAll()
+                            .requestMatchers( "/banks").hasRole("managers")
+                            .requestMatchers(HttpMethod.POST, "/users").hasRole("managers")
+                            .requestMatchers(HttpMethod.GET, "/users").hasAnyRole("managers", "users")
+                            .requestMatchers(HttpMethod.PUT, "/users").hasRole("users")
+                            .requestMatchers( "/accounts").hasRole("users")
+                            .requestMatchers(HttpMethod.POST, "/transaction").hasRole("users")
+                            .requestMatchers(HttpMethod.GET, "/transaction").hasAnyRole("managers", "users")
+                            .requestMatchers(HttpMethod.PUT, "/transaction").hasRole("managers")
                             .anyRequest()
-                            .authenticated()
-            ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                            .denyAll()
+            ).oauth2ResourceServer(auth -> auth.jwt().jwtAuthenticationConverter(getJwtAuthenticationConverter()));
 
     return http.build();
+  }
+
+  public Converter<Jwt, AbstractAuthenticationToken> getJwtAuthenticationConverter() {
+    JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+    converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+      Collection<GrantedAuthority> authorities = new java.util.ArrayList<>(Collections.emptyList());
+      Collection<String> roleClaim = (List<String>) jwt.getClaimAsMap("realm_access").get("roles");
+      authorities.addAll(roleClaim.stream()
+              .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+              .collect(Collectors.toSet()));
+      return authorities;
+    });
+    return converter;
   }
 }
