78abec353644e865a3f78185f0c15ec35e72809c
juanFeReyes <juan.felipe.reyesg@gmail.com>
2025-02-07 20:08:14 -0500

    feat: solve subsets

---
+++ java/tech-problems/src/main/java/leetCode/medium/Subsets.java
@@ -0,0 +1,29 @@
+package leetCode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+
+    List<List<Integer>> permutations;
+
+    public List<List<Integer>> subsets(int[] nums) {
+        permutations = new ArrayList<>();
+
+        permute(nums, 0, new ArrayList<>());
+
+        return permutations;
+    }
+
+    private void permute(int[] nums, int idx, List<Integer> sub){
+
+        permutations.add(new ArrayList<>(sub));
+
+        for (int i = idx; i < nums.length; i++) {
+            List<Integer> cloneSub = new ArrayList<>(sub);
+            int current = nums[i];
+            cloneSub.add(current);
+            permute(nums, i+1, cloneSub);
+        }
+    }
+}
