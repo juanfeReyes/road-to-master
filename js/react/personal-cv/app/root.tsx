001c2148ab6287d0375224974da3921116978240
juanFeReyes <juan.felipe.reyesg@gmail.com>
2025-07-13 17:18:09 -0500

    fix: fixing asset routing

--- js/react/personal-cv/app/root.tsx
+++ js/react/personal-cv/app/root.tsx
@@ -39,7 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <base href="/" />
+        <base href="/docs/" />
       </head>
       <body>
         {children}
